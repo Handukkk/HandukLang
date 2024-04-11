c2be237dabe5ce82e715763cea5051cd49487a71
Johan <johandvn05@gmail.com>
2024-04-11 15:57:27 +0700

    make visitor

---
+++ src/include/visitor.h
@@ -0,0 +1,19 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+#include "ast.h"
+
+ast_T* visitor_visit(ast_T* node);
+
+ast_T* visitor_visit_var_def(ast_T* node);
+
+ast_T* visitor_visit_var_call(ast_T* node);
+
+ast_T* visitor_visit_func_def(ast_T* node);
+
+ast_T* visitor_visit_func_call(ast_T* node);
+
+ast_T* visitor_visit_string(ast_T* node);
+
+ast_T* visitor_visit_compound(ast_T* node);
+
+#endif
